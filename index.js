605b744ccd7909e09060053d306e6b04307b5e75
binarykittenDE <franziska.haaf@gmail.com>
2018-10-10 14:00:45 +0200

    Implementation. Now its functional.

--- index.js
+++ index.js
@@ -2,127 +2,105 @@
  * Created by franziskah on 09.10.18.
  */
 
-let Twit = require('twit');
-let TwitterBot = require('node-twitterbot').TwitterBot;
+var TwitterPackage = require('twitter');
 
-let Bot = new TwitterBot({
-    consumer_key: process.env.BOT_CONSUMER_KEY,
-    consumer_secret: process.env.BOT_CONSUMER_SECRET,
-    access_token: process.env.BOT_ACCESS_TOKEN,
-    access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
-});
+let Bot = new TwitterPackage({
+ consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+ });
 
-const BODY_PARTS = ['Ankle', 'Arm', 'Back', 'Bicep', 'Buttocks', 'Calf', 'Chest', 'Right Ear', 'Left Ear', 'Face', 'Foot',
-    'Forearm', 'Full back', 'Full leg', 'Half sleeve', 'Hand', 'Head', 'Hip', 'Knee', 'Leg', 'Lip', 'Lower Back',
-    'Mouth', 'Neck', 'Ribcage', 'Shoulder', 'Side', 'Sleeve', 'Sternum', 'Stomach', 'Thigh', 'Torso', 'Upper back', 'Wrist'];
+const BODY_PARTS = ['ankle', 'arm', 'back', 'bicep', 'buttocks', 'calf', 'chest', 'right ear', 'left ear', 'face', 'foot',
+    'forearm', 'full back', 'full leg', 'half sleeve', 'hand', 'head', 'hip', 'knee', 'leg', 'lip', 'lower back',
+    'mouth', 'neck', 'ribcage', 'shoulder', 'side', 'sleeve', 'sternum', 'stomach', 'thigh', 'torso', 'upper back', 'wrist'];
 
-const TATTOO_STYLES = ['Abstract', 'Armband', 'Baroque', 'Bio-mechanical', 'Bio-organic', 'Black & Grey', 'All black', 'Botanical',
-    'Cartoon', 'Celtic', 'Chinese', 'Classic', 'Color', 'Couples', 'Dotwork', 'Fantasy', 'Geometric', 'Graffiti', 'Illusion', 'Illustrative',
-    'Lettering', 'Linework', 'Macabre', 'Minimalism', 'Neo-Traditional', 'New School', 'Old School', 'Ornamental', 'Portrait', 'Realistic', 'Religious',
-    'Quote', 'Sexy', 'Surrealism', 'Traditional', 'Tribal', 'UV light', 'Watercolor', 'White ink'];
+const TATTOO_STYLES = ['abstract', 'armband', 'baroque', 'bio-mechanical', 'bio-organic', 'black & grey', 'all black', 'botanical',
+    'cartoon', 'celtic', 'chinese', 'classic', 'color', 'couples', 'dotwork', 'fantasy', 'geometric', 'graffiti', 'illusion', 'illustrative',
+    'lettering', 'linework', 'macabre', 'minimalism', 'neo-traditional', 'new School', 'old school', 'ornamental', 'realistic', 'religious',
+    'quote', 'sexy', 'surrealism', 'traditional', 'tribal', 'UV light', 'watercolor', 'white ink'];
 
-const MOTIVES = ['Eagle', 'Knife', 'Heart', 'Skull', 'Rainbow', 'Bird', 'Wolf', 'Dragon', 'Dolphin', 'Star', 'Zodiac', 'Biker',
-    'I Love Mom', 'Wind Rose', 'Arrow', 'An Astronaut being abducted by an alien', 'Sonic', 'Cracked Woman Head With A Forest',
-    'Tiny Spider-Man Head', 'Black Octopus', 'Cthulhu', 'Dove', 'Wonder Woman', 'Tiger', 'Joker from Batman', 'Panther doing acid',
-    'Floating Astronauts Skull', 'Roaring Gorilla Head', 'all seeing eye', 'Burning Church', 'Death Star', 'A little cow',
-    'An amazing horse so people would look at it', '20 sided die', 'd20', 'Rainbow', 'Bongo Cat', 'Constellation', 'Planet', 'Galaxy',
-    'Compass', 'Butterfly', 'Cross', 'Hexagram', 'Tribal', 'Book', 'Flower', 'Dandelion', 'Wings', 'Snake', 'Fox', 'Joker', 'Salvador Dalí: The Elephants',
-    'Hourglass', 'Ship', 'Mountains', 'Abstract Punctuation', 'Mandala', 'Unicorn', 'Football banter', 'Sword', 'Superhero', 'Renew your mind', 'Be Nice!', 'Infinity',
-    'Sun', 'Moon', 'Cat', 'Your dead pet', 'Chinese Sign'];
+const MOTIVES = ['eagle', 'knife', 'heart', 'skull', 'rainbow', 'bird', 'wolf', 'dragon', 'dolphin', 'star', 'zodiacsign', 'biker',
+    'I-Love-Mom-Banner', 'wind rose', 'arrow', 'astronaut being abducted by an alien', 'sonic',
+    'tiny Spider-Man head', 'black octopus', 'Cthulhu', 'dove', 'Wonder Woman', 'tiger', 'Joker from Batman', 'panther doing acid',
+    'floating astronauts skull', 'roaring gorilla head', 'all seeing eye', 'burning church', 'death Star', 'little' +
+    ' cow',
+    'amazing horse, so people would look at it', '20 sided die', 'd20', 'rainbow', 'bongo playig cat', 'constellation', 'planet', 'galaxy',
+    'compass', 'butterfly', 'cross', 'hexagram', 'tribal', 'book', 'flower', 'dandelion', 'wings', 'snake', 'fox', 'joker', 'Salvador Dalís "The Elephants"',
+    'hourglass', 'ship', 'mountains', 'mandala', 'unicorn', 'football banter', 'sword', 'superhero', 'Be-Nice-Speech-Bubble', 'infinity sign',
+    'sun', 'moon', 'cat', 'dead pet of your past', 'chinese sign', 'star', 'table', 'chair', 'fridge', 'plant', 'palm tree', 'ocean', 'towel', 'snake', 'cobra', 'Jesus',
+    'Harry Potter lightning', 'masks', 'Spyro', 'portrait of your dad', 'portrait of your mum', 'portrait of your' +
+    ' firstborn', 'portrait of yourself', 'eye', 'hentai motive', 'anime figure', 'comic figure', 'bart simpson', 'lisa simpson', 'female power', 'shell',
+    'bone', 'foot', 'hand', 'goddess', 'god', 'indian god', 'buddha', 'ace', 'LGBT* motive', 'cross', 'country', 'flag', 'butterfly', 'fish', 'rose', 'number 1', 'monster,',
+    'monstera', 'Mickey Mouse', 'Minney Mouse', 'Garfield', 'crystal', 'tree', 'flame', 'icecube', 'rapper', 'DJ', 'popstar', 'politician', 'lion', 'panther', 'acovado',
+    'banana', 'flying bird', 'car', 'car brand logo', 'logo', 'cocktail', 'whine glass', 'wine bottle', 'alcohol' +
+    ' bottle', 'name', 'name of your ex', 'superhero'];
+
+const MAX_AMOUNT_OF_MOTIVES = 3;
+
+const HASHTAGS = '#yourNextTattoo #tattoo #tattoos #tat #ink #inked #tattooed #tattooIdea #tattooIdeas';
 
 /**
- * Master function to generate all motives required to provide
- * a tattoo result.
- * @param {Array} motives array of motives to choose from.
+ *
+ * @returns {Array} possible motives
  */
-function getMotives(motives) {
-    let motives = motives.slice();
-    let selected = [];
-    const maxNoOfMotives = 3;
-    // Always select at least 1 motive.
-    selected.push(getMotive(motives));
+function getMotives() {
+    //choose 1 to 3
+    let motiveAmount = randomIntFromInterval(1, MAX_AMOUNT_OF_MOTIVES);
+    let chosenMotives = [];
+    let newMotive;
 
-    // Selection of subsequent motives is random. Use maxNoOfMotives to
-    // determine max length of returned motives.
-    for (let i = 0; i < maxNoOfMotives - 1; i++) {
-        if (Math.round(Math.random()) === 1) {
-            // Removes the last selected motive from the array.
-            motives = removeArrayValue(motives, selected[selected.length - 1]);
-            selected.push(getMotive(motives));
-        }
+    for (let i = 0; i < motiveAmount; i++) {
+        do {
+            newMotive = (MOTIVES[randomIntFromInterval(1, MOTIVES.length)]);
+        } while (chosenMotives.includes(newMotive));
+
+        chosenMotives.push(newMotive);
     }
-
-    while (selected.length < maxNoOfMotives) {
-        selected.push('---');
-    }
-
-    return selected;
+    return chosenMotives;
 }
 
-/**
- * Returns a possible tattoo motive.
- * Motives are passed in rather than using global array to avoid
- * motive double ups.
- *
- * @param {Array} motives Array of motives to choose from.
- */
-function getMotive(motives) {
-    return motives[Math.floor(Math.random() * motives.length)];
+//including min and max
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
 /**
  * Returns a possible tattoo style.
- *
- * @param {Array} tattooStyles Array of tattoo styles to choose from.
  */
-function getTattooStyle(tattooStyles) {
-    console.log('Im getTatttoStyle');
-    let tattooStyles = tattooStyles.slice();
-    return tattooStyles[Math.floor(Math.random() * tattooStyles.length)];
+function getTattooStyle() {
+    return TATTOO_STYLES[randomIntFromInterval(1, TATTOO_STYLES.length)];
 }
 
 /**
  * Returns a possible body part.
- *
- * @param {Array} bodyPart Array of body parts to choose from.
  */
-function getBodyPart(bodyPart) {
-    let bodyPart = bodyPart.slice();
-    return bodyPart[Math.floor(Math.random() * bodyPart.length)];
+function getBodyPart() {
+    return BODY_PARTS[randomIntFromInterval(1, BODY_PARTS.length)];
 }
-
-/**
- * Removes a value from an array and returns a new array.
- *
- * @param {Array} array  Array of values to be searched.
- * @param {String} value Value to be removed from Array.
- */
-function removeArrayValue(array, value) {
-    const valueIndex = array.indexOf(value);
-
-    // If value found in array:
-    if (valueIndex > -1) {
-        array.splice(valueIndex);
-    }
-
-    return array;
-}
-
 
 /**
  * Generates a random tattoo design with a relevant color, and 2 motives.
  */
 function generateTattoo() {
-    const tattooStyle = getTattooStyle(TATTOO_STYLES);
-    const motives = getMotives(MOTIVES);
-    const bodyPart = getBodyPart(BODY_PARTS);
-    let motivesString = '';
-    for (let i = 0; i < motives.length; i++) {
-        motivesString = motivesString + ' + ' + motives[i];
-    }
-    return tattooStyle + ' ' + motivesString + 'on ' + bodyPart;
+    const tattooStyle = getTattooStyle();
+    const motives = getMotives();
+    const bodyPart = getBodyPart();
+
+    return 'Your next tattoo should be a/an ' + tattooStyle + ' tattoo that shows a/an ' + motives.join(' and ') + ' on' +
+        ' your ' + bodyPart + ' ' + HASHTAGS;
 }
 
 let generatedTattoo = generateTattoo();
 
-Bot.tweet('This is a test.');
+console.log(generatedTattoo);
+
+Bot.post('statuses/update', {status: generatedTattoo}, function (error, tweet, response) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(tweet);
+    console.log(response);
+});
+
+
